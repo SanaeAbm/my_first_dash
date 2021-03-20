62c5ace3d64025757afe3d5d344770806867003b
SanaeAbm <sanae.abm@gmail.com>
2021-03-20 13:11:17 +0100

    styling

--- app.py
+++ app.py
@@ -13,6 +13,15 @@ df = pd.read_csv(df_url)
 df_vore = df['vore'].dropna().sort_values().unique()
 opt_vore = [{'label': x + 'vore', 'value': x} for x in df_vore]
 
+colors = {
+     'background': '#7FDBFF',
+     'text': '#745D34',
+     'special': 'purple'
+ }
+
+
+
+
 app = dash.Dash(__name__, title="My first Dash Python App")
 
 
@@ -24,24 +33,35 @@ markdown_text = '''
 '''
 
 app.layout = html.Div([
-    html.H1(app.title),
+    html.Div([html.H1(app.title,className="app-header-title")],
+    className="app-header"),
+    html.Div([
     dcc.Markdown(markdown_text),
     html.Label(["Select types of feeding strategies:", 
-        dcc.Dropdown('my-dropdown', options= opt_vore, value= opt_vore[0]['value'])
+       dcc.Dropdown('my-dropdown',
+            options= opt_vore, 
+            value= [opt_vore[0]['value']],
+            multi=True
+        )
     ]),
     dt.DataTable(
         id='my-table',
         columns=[{"name": i, "id": i} for i in df.columns],
         data= df.to_dict("records")
         )
-])
+     ], className= "app-body")
+    ])
+
+ 
+
+
 
 
 @app.callback(
      Output('my-table', 'data'),
      Input('my-dropdown', 'value'))
-def update_data(value):
-    filter = df['vore'] == value
+def update_data(values):
+    filter = df['vore'].isin(values)
     return df[filter].to_dict("records")
 
 if __name__ == '__main__':
